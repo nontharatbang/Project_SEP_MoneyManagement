040d04a9daaea63583ce83d1e97bbbab0ecb737a
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2022-05-26 15:01:01 +0700

    commit

--- PassiveMoney.py
+++ PassiveMoney.py
@@ -1,1 +1,8 @@
-class 
+class PassiveMoney:
+    def __init__(self, money, category, memo, start_date, end_date, frequency, count):
+        self.money = money
+        self.category = category
+        self.memo = memo
+
+    def is_negative():
+        pass
